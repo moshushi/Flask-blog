43a9eb4b787032808bfa3a6bbd7ef74a4ea7ae7c
moshushi <moshushi@gmail.com>
2018-05-24 23:03:20 +0300

    2th itertion on Flask-microblog

---
+++ app/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# mail = Mail
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+mail = Mail
+bootstrap = Bootstrap(app)
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(mailhost=(
+            app.config['MAIL_SERVER'],
+            app.config['MAIL_PORT']
+        ),
+            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs = app.config['ADMINS'], subject = 'Microblog Failure',
+            credentials=auth, secure = secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(actime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+from app import routes, models, errors
